fb39139f9c0afea98c1a27e869bd302ba1201268
Ignacio Cinalli <nachocinalli@gmail.com>
2021-11-12 17:34:07 -0300

    Set the number of columns from the component model

--- js/matchingGraphicView.js
+++ js/matchingGraphicView.js
@@ -1,15 +1,30 @@
 import MatchingView from 'components/adapt-contrib-matching/js/matchingView';
-
+import Adapt from 'core/js/adapt';
 class MatchingGraphicView extends MatchingView {
 
   onQuestionRendered() {
-    this.$('.matching__item-graphic').imageready(() => {
-      this.setReadyStatus();
-    });
-
+    this.setUpColumns();
     this.setUpDropdowns();
+    this.$('.matching__item-graphic').imageready(() => this.setReadyStatus());
+    this.listenTo(Adapt, {
+      'device:resize': this.onDeviceResize
+    });
   }
 
+  onDeviceResize() {
+    this.setUpColumns();
+  }
+
+  setUpColumns() {
+    const columns = this.model.get('_columns');
+
+    if (!columns) return;
+
+    const isLarge = Adapt.device.screenSize === 'large';
+
+    this.$el.toggleClass('has-column-layout', isLarge);
+    this.$('.matching__item').css('width', isLarge ? (100 / columns) + '%' : '');
+  }
 }
 MatchingGraphicView.template = 'matching-graphic';
 
