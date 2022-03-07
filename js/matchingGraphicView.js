7f93263c0cf4815c3c90aa6e9103b14defb8422f
Ignacio Cinalli <nachocinalli@gmail.com>
2022-03-07 17:50:42 -0300

    Support Matching v6.0.0

--- js/matchingGraphicView.js
+++ js/matchingGraphicView.js
@@ -1,31 +1,11 @@
 import MatchingView from 'components/adapt-contrib-matching/js/matchingView';
-import Adapt from 'core/js/adapt';
+
 class MatchingGraphicView extends MatchingView {
 
   onQuestionRendered() {
-    this.setUpColumns();
-    this.setUpDropdowns();
-    this.$('.matching__item-graphic').imageready(() => this.setReadyStatus());
-    this.listenTo(Adapt, {
-      'device:resize': this.onDeviceResize
-    });
-  }
-
-  onDeviceResize() {
-    this.setUpColumns();
-  }
-
-  setUpColumns() {
-    const columns = this.model.get('_columns');
-
-    if (!columns) return;
-
-    const isLarge = Adapt.device.screenSize === 'large';
-
-    this.$el.toggleClass('has-column-layout', isLarge);
-    this.$('.matching__item').css('width', isLarge ? (100 / columns) + '%' : '');
+    this.$('.matching-item__image-container').imageready(() => this.setReadyStatus());
   }
 }
-MatchingGraphicView.template = 'matching-graphic';
+MatchingGraphicView.template = 'matchingGraphic.jsx';
 
 export default MatchingGraphicView;
---
+++ templates/matchingGraphic.jsx
@@ -0,0 +1,88 @@
+import Adapt from 'core/js/adapt';
+import React from 'react';
+import { templates, classes } from 'core/js/reactHelpers';
+
+export default function MatchingGraphic(props) {
+  const {
+    _isEnabled,
+    _isInteractionComplete,
+    _isCorrect,
+    _canShowMarking,
+    _isCorrectAnswerShown,
+    _columns,
+    _items,
+    _options
+  } = props;
+
+  const displayAsCorrect = (_isInteractionComplete && (_isCorrectAnswerShown || _isCorrect));
+  const hasColumns = _columns > 1;
+  return (
+    <div className="component__inner matching__inner">
+
+      <templates.header {...props} />
+
+      <div className={classes([
+        'component__widget matching__widget',
+        !_isEnabled && 'is-disabled',
+        _isInteractionComplete && 'is-complete is-submitted show-user-answer',
+        displayAsCorrect && 'is-correct',
+        hasColumns && 'has-columns'
+      ])}>
+
+        {_items.map(({
+          text,
+          _graphic,
+          _index
+        }, index) => {
+          const activeOption = _options.find(option => (option._itemIndex === _index) && option._isActive);
+          const displayItemAsCorrect = (!_isEnabled && _canShowMarking && (_isCorrectAnswerShown || activeOption?._shouldBeSelected));
+          return (
+            <div key={_index} style={(hasColumns && Adapt.device.screenSize === 'large' && { width: `${100 / _columns}%` }) || null} className={classes([
+              'matching-item',
+              'item',
+              `item-${index}`,
+              'js-matching-item',
+              displayItemAsCorrect ? 'is-correct' : 'is-incorrect'
+            ])
+
+            }>
+
+              {text &&
+              <div className="matching-item__title">
+                <div className="matching-item__title_inner" dangerouslySetInnerHTML={{ __html: text }}>
+                </div>
+              </div>
+              }
+              {_graphic &&
+              <templates.image {..._graphic}
+                classNamePrefixes={[
+                  'matching-item'
+                ]}
+              />
+              }
+              <div className="matching-item__select-container js-matching-item-select-container">
+
+                <templates.matchingDropDown {...props} _itemIndex={_index} />
+
+                <div className="matching-item__select-state">
+                  <div className="matching-item__select-icon matching-item__select-correct-icon">
+                    <div className="icon"></div>
+                  </div>
+                  <div className="matching-item__select-icon matching-item__select-incorrect-icon">
+                    <div className="icon"></div>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+          );
+        })}
+
+      </div>
+
+      <div className="btn__container"></div>
+
+    </div>
+  );
+}
