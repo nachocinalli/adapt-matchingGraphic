a7b798e2c72a1288ae27660a3134ccca39bf9e6a
Ignacio Cinalli <nachocinalli@gmail.com>
2022-05-17 15:23:11 -0300

    Add randomize items, closes #1, closes #2

--- js/matchingGraphicView.js
+++ js/matchingGraphicView.js
@@ -1,6 +1,14 @@
 import MatchingView from 'components/adapt-contrib-matching/js/MatchingView';
 
 class MatchingGraphicView extends MatchingView {
+  setupQuestion() {
+    if (this.model.get('_isItemsRandom') && this.model.get('_isEnabled')) {
+      const items = _.shuffle(this.model.get('_items'));
+      this.model.set('_items', items);
+    }
+    this.model.setupRandomisation();
+    this.model.setupInitialHighlighted();
+  }
 
   onQuestionRendered() {
     this.$('.matching-item__image-container').imageready(() => this.setReadyStatus());
